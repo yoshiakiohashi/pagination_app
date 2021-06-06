1dba7a669e783a468b1ac7d4b11c099cd6fc8bfa
yoshiaki ohashi <ohashi.spdc@icloud.com>
2021-06-06 11:02:28 +0900

    ページネーションを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  PER_PAGE = 10
+
   def index
-    @posts = Post.order(id: :desc)
+    @posts = Post.order(id: :desc).page(params[:page]).per(PER_PAGE)
   end
 
   def create
